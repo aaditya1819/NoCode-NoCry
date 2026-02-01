85b68c3b3a9bc47e7a17dbedbf65c385952b0174
Aaditya Dhanwate <aadityadhanwate830@gmail.com>
2026-02-01 20:05:51 +0530

    fix: use default prisma client output for vercel

--- server/lib/prisma.ts
+++ server/lib/prisma.ts
@@ -1,7 +1,7 @@
 import "dotenv/config";
 import pg from "pg";
 import { PrismaPg } from '@prisma/adapter-pg'
-import { PrismaClient } from '../generated/prisma/client.js'
+import { PrismaClient } from '@prisma/client'
 
 if (!process.env.DATABASE_URL) {
     throw new Error("DATABASE_URL is not defined in environment variables");
